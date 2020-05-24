8c51079690a5d862cedaae20a278a1ee114e0274
David Wang <davidwang200099@163.com>
2020-05-23 20:49:51 -0400

    Add files via upload

--- csim.c
+++ csim.c
@@ -10,6 +10,13 @@ int associativity=0;
 int blockBits=0;
 char *traceName=NULL;
 
+int setSize;
+int blockSize;
+
+int hitTime=0;;
+int missTime=0;
+int evictionTime=0;
+
 void printHelpInfo(){
     printf("./csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
     printf("\t-v\tOptional verbose flag that displays trace info\n");
@@ -19,7 +26,7 @@ void printHelpInfo(){
     printf("\t-t <tracefile>\tName of valgrind trace to display\n");
 }
 
-int getBlockNumber(long address,int blockBits){
+int getBlockNumber(unsigned long address,int blockBits){
     int mask=1;
     int i;
     for(i=1;i<=blockBits-1;i++){
@@ -29,19 +36,43 @@ int getBlockNumber(long address,int blockBits){
     return address&mask;
 }
 
-int getSetNumber(long address,int setBits,int blockBits){
+int getSetNumber(unsigned long address,int setBits,int blockBits){
     int mask=1<<blockBits;
     int i;
     for(i=1;i<=setBits-1;i++){
         mask<<=1;
         mask+=(1<<blockBits);
     }
-    return address&mask;
+    return (address&mask)>>blockBits;
 }
-void store(char *cache,long address,int oprtsize){
-    int setsize=associativity*(2+64+(1<<blockBits));
+
+int getTagNumber(unsigned long address,int setBits,int blockBits){
+    int mask=1;
+    for(i=1;i<=setBits+blockBits-1;i++){
+        mask<<=1;
+        mask+=1;
+    }
+    mask=~mask;
+    return (address&mask)>>(setBits+blockBits);
+}
+
+void store(char v,char *cache,unsigned long address,int oprtsize){
     int setNumber=getSetNumber(address,setBits,blockBits);
-    
+    char *tagptr=cache+setSize*setNumber;
+    char flag=0;
+    int i;
+    for(i=0;i<associativity;i++){
+        if( *(int *)tagptr==setNumber && *(tagptr+1)) {flag=1;break;}
+    }
+    if(!v){
+        if(flag) {
+            hitTime++;
+            printf(" S %x,%d hit\n",address,oprtsize);
+        }
+        else {
+            missTime++;
+        }
+    }
 }
 
 int main(int argc,char **argv){
@@ -95,17 +126,27 @@ int main(int argc,char **argv){
         return 0;
     }
     int cachesize;
-    char *cache=(char *)malloc(cachesize=(1<<setBits)*associativity*((1<<blockBits)+2+sizeof(long)));
-    // block structure: validbit:dirtybit:tag:bytes
+    char *cache=(char *)malloc(cachesize=((1<<setBits))*
+                                        (associativity)*
+                                        (blockSize=(1<<blockBits)+4+sizeof(int)));
+    setSize=blockSize*associativity;
+
+    // block structure: tag[4]:validbit[1]:dirtybit[1]:times[2]:bytes
     memset(cache,0,cachesize);
     char order,comma;
-    long address,oprtsize;
+    unsigned long address,oprtsize;
     while(!feof(trace)){
         fscanf(trace,"%c%x%c%d",&order,&address,&comma,&oprtsize);
         switch(order){
             case 'I':break;
             case 'S':
-                printf("S %x%c%d\n",address,comma,oprtsize);
+                //printf("S %x%c%d\n",address,comma,oprtsize);
+                if(flag){
+
+                }else{
+                    if(store(cache,address,oprtsize)) hitTime++;
+                    else missTime++;
+                }
                 break;
             case 'L':
                 printf("L %x%c%d\n",address,comma,oprtsize);
